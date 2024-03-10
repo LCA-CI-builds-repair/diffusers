2b44ea74ba33c58f64c5d718171c533ed81ae071
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-03-10 16:14:32 +0000

    test_lca_code_editing__code-editing-metrics__id_5

--- src/diffusers/loaders/ip_adapter.py
+++ src/diffusers/loaders/ip_adapter.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-from typing import Dict, Optional, Union
-
-import torch
 from huggingface_hub.utils import validate_hf_hub_args
+from typing import Dict, Optional, Union
 from safetensors import safe_open
 
 from ..utils import (
