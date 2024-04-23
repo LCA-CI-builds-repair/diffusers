9abc044cd1e8f784b7e1b2c2bb633ead6b5fd04f
ubuntu <ubuntu@ip-10-0-0-33>
2024-04-23 23:42:06 +0000

    test_lca_code_editing__code-editing-metrics-x3ee70613189c9946__id_5

--- examples/inference/image_to_image.py
+++ examples/inference/image_to_image.py
@@ -1,4 +1,8 @@
-import warnings
+impfrom diffusers import StableDiffusionImg2ImgPipeline
+warnings.warn(
+    "The `image_to_image.py` script is outdated. Please use directly `from diffusers import"
+    " StableDiffusionImg2ImgPipeline` instead."
+)rnings
 
 from diffusers import StableDiffusionImg2ImgPipeline  # noqa F401
 
