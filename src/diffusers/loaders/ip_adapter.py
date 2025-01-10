a3c6d6b6d18ccf060be1732502dc4a1524623407
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 12:20:44 +0000

    waleko__gemini-pro-1.5_3bee__id_5

--- src/diffusers/loaders/ip_adapter.py
+++ src/diffusers/loaders/ip_adapter.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 import os
 from typing import Dict, Optional, Union
-
+from typing import Dict, Union
 import torch
 from huggingface_hub.utils import validate_hf_hub_args
 from safetensors import safe_open
