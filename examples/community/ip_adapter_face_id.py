4f6feb9da45e62e1eb28703bb9225ca2e1f1bc80
ubuntu <ubuntu@ip-10-0-1-60>
2025-01-12 15:25:14 +0000

    waleko__claude-3-opus_8820__id_4

--- examples/community/ip_adapter_face_id.py
+++ examples/community/ip_adapter_face_id.py
@@ -11,9 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Any, Callable, Dict, List, Optional, Union
 
 import inspect
-from typing import Any, Callable, Dict, List, Optional, Union
 from safetensors import safe_open
 
 import torch
