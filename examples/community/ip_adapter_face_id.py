2d4f744c4c875ca490b50ff683fbe8503fc83735
ubuntu <ubuntu@ip-10-0-1-7>
2025-01-16 01:49:18 +0000

    waleko__llama-3.1-70b_6b88__id_4

--- examples/community/ip_adapter_face_id.py
+++ examples/community/ip_adapter_face_id.py
@@ -13,8 +13,24 @@
 # limitations under the License.
 
 import inspect
+from packaging import version
 from typing import Any, Callable, Dict, List, Optional, Union
-from safetensors import safe_open
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from diffusers.configuration_utils import FrozenDict
+from diffusers.image_processor import VaeImageProcessor
+from diffusers.loaders import FromSingleFileMixin, IPAdapterMixin, LoraLoaderMixin, TextualInversionLoaderMixin
+from diffusers.models import AutoencoderKL, UNet2DConditionModel
+from diffusers.models.attention_processor import FusedAttnProcessor2_0
+from diffusers.models.lora import adjust_lora_scale_text_encoder, LoRALinearLayer
+from diffusers.schedulers import KarrasDiffusionSchedulers
+from diffusers.utils import _get_model_file, USE_PEFT_BACKEND, deprecate, logging, scale_lora_layers, unscale_lora_layers
+from diffusers.utils.torch_utils import randn_tensor
+from diffusers.pipelines.pipeline_utils import DiffusionPipeline
+from diffusers.pipelines.stable_diffusion.pipeline_output import StableDiffusionPipelineOutput
+from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+from transformers import CLIPImageProcessor, CLIPTextModel, CLIPTokenizer, CLIPVisionModelWithProjection
 
 import torch
 import torch.nn as nn
