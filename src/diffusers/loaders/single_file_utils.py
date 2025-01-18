e92c185cfdcf608d9111447c6ad9fd56beaf0d5d
ubuntu <ubuntu@ip-10-0-1-44>
2025-01-18 14:25:54 +0000

    waleko__claude-3.5-sonnet_f18c__id_44

--- src/diffusers/loaders/single_file_utils.py
+++ src/diffusers/loaders/single_file_utils.py
@@ -20,9 +20,8 @@ from contextlib import nullcontext
 from io import BytesIO
 
 import requests
-import torch
 import yaml
-from safetensors.torch import load_file as safe_load
+from safetensors.torch import load_file
 from transformers import (
     CLIPTextConfig,
     CLIPTextModel,
