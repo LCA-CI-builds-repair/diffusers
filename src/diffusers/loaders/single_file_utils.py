689139a37986045dd8afde5efb80f51132a98731
ubuntu <ubuntu@ip-10-0-1-43>
2025-01-30 15:06:14 +0000

    waleko__deepseek-r1_df21__id_44

--- src/diffusers/loaders/single_file_utils.py
+++ src/diffusers/loaders/single_file_utils.py
@@ -20,13 +20,11 @@ from contextlib import nullcontext
 from io import BytesIO
 
 import requests
-import torch
 import yaml
 from safetensors.torch import load_file as safe_load
 from transformers import (
     CLIPTextConfig,
     CLIPTextModel,
-    CLIPTextModelWithProjection,
     CLIPTokenizer,
 )
 
